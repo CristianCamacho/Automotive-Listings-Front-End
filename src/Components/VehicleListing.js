78631255e187eb794374a748d47c200999ecef88
Cristian Camacho <Cristian_Camacho@aol.com>
2022-02-14 14:29:53 -0500

    Added Models and Options selection.

--- src/Components/VehicleListing.js
+++ src/Components/VehicleListing.js
@@ -8,7 +8,11 @@ class VehicleListing extends Component {
             years: [],
             makes: [],
             models: [],
-            options: []
+            options: [],
+            selYear: '',
+            selMake: '',
+            selModel: '',
+            selOption: ''
         }
     }
 
@@ -28,23 +32,55 @@ class VehicleListing extends Component {
 
     componentDidUpdate() {
         console.log('did update')
-        if(this.state.makes.length < 1 && this.state.selYear > 1983) {
-            console.log('get makes')
-            fetch('http://localhost:5000/api/v1/auto_info/get_makes?year=' + this.state.selYear)
-                .then(res => {
-                    return res.json()
-                }).then(data => {
-                    this.setState({
-                        makes: data.makes
+        console.log(this.state)
+        if (this.state.selYear) {
+            if (this.state.makes.length < 1) {
+                console.log('get makes')
+                fetch('http://localhost:5000/api/v1/auto_info/get_makes?year=' + this.state.selYear)
+                    .then(res => {
+                        return res.json()
+                    }).then(data => {
+                        this.setState({
+                            makes: data.makes
+                        })
                     })
-                })
+            } else if (this.state.selMake && this.state.models.length < 1) {
+                console.log('get models')
+                fetch('http://localhost:5000/api/v1/auto_info/get_models?year=' + this.state.selYear + '&make=' + this.state.selMake)
+                    .then(res => {
+                        return res.json()
+                    }).then(data => {
+                        console.log(data)
+                        this.setState({
+                            models: data.models
+                        })
+                    })
+                console.log(this.state.models)
+            } else if (this.state.selMake && this.state.selModel && this.state.options.length < 1) {
+                console.log('get options')
+                fetch('http://localhost:5000/api/v1/auto_info/get_options?year=' + this.state.selYear + '&make=' + this.state.selMake + '&model=' + this.state.selModel)
+                    .then(res => {
+                        return res.json()
+                    }).then(data => {
+                        console.log(data)
+                        this.setState({
+                            options: data.options
+                        })
+                    })
+            }
         }
+
     }
 
     handleChange = (event) => {
-        if(event.target.id == 'selYear') {
+        if (event.target.id == 'selYear') {
             this.setState({
-                makes: []
+                makes: [],
+                models: [],
+                options: [],
+                selMake: '',
+                selModel: '',
+                selOption: ''
             })
         }
         this.setState({
@@ -53,7 +89,6 @@ class VehicleListing extends Component {
     }
 
     render() {
-        console.log(this.state.year)
         return (
             <div>
                 <form >
@@ -65,11 +100,27 @@ class VehicleListing extends Component {
                             })
                         }
                     </Select>
-                    <Select id='selMake' labl='Select Makes' onChange={this.handleChange}>
+                    <Select id='selMake' labl='Select Make' onChange={this.handleChange}>
                         <Select.Option>Select Makes</Select.Option>
                         {
-                            this.state.makes.map((makes) => {
-                                return (<Select.Option>{makes}</Select.Option>)
+                            this.state.makes.map((make) => {
+                                return (<Select.Option>{make}</Select.Option>)
+                            })
+                        }
+                    </Select>
+                    <Select id='selModel' labl='Select Model' onChange={this.handleChange}>
+                        <Select.Option>Select Model</Select.Option>
+                        {
+                            this.state.models.map((model) => {
+                                return (<Select.Option>{model}</Select.Option>)
+                            })
+                        }
+                    </Select>
+                    <Select id='selOption' labl='Select Option' onChange={this.handleChange}>
+                        <Select.Option>Select Option</Select.Option>
+                        {
+                            this.state.options.map((option) => {
+                                return (<Select.Option>{option}</Select.Option>)
                             })
                         }
                     </Select>
