7320b2a4c64a79400afede9b0065c1bee8ffb85b
Cristian Camacho <Cristian_Camacho@aol.com>
2022-03-07 13:37:58 -0500

    Quickly added a sign up page, mimicks sign in page. No functionality.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import LandingPage from './Components/LandingPage'
 import SignInPage from './Components/SignInPage'
+import SignUp from './Components/SignUpPage'
 
 class App extends Component {
   render() {
     return (
-      <SignInPage />
+      <SignUp />
     )
   }
 }
---
+++ src/Components/SignUpPage.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+import Header from "./Header"
+import { Input, Button } from '@supabase/ui'
+
+class SignUp extends Component {
+
+    handleOnChange = (event) => {
+        [event.target.id] = event.target.value
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <div class='flex items-center justify-center flex-col'>
+                    <form class='w-3/4 lg:w-2/3 px-8 py-10 flex items-center justify-center flex-col'>
+                        <div className='w-2/4 flex-col justify-items-center'>
+                            <Input className='mb-6' label='User' onChange={this.handleOnChange} type='text' name='username' id='username' />
+                            <Input className='mb-6' label='Password' onChange={this.handleOnChange} type='password' name='password' id='password' />
+                            <Input className='mb-6' label='Verify Password' onChange={this.handleOnChange} type='password' name='vPassword' id='vPassword' />
+                        </div>
+                        <div className='flex flex-row'>
+                            <div>
+                                <Button className='m-2' >Create Account</Button>
+                            </div>
+                            <div>
+                                <Button className='m-2' >Sign In</Button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SignUp
