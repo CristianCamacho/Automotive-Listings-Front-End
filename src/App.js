d55eb4698207f1f973a731ab5cb877dbf054448e
Cristian Camacho <Cristian_Camacho@aol.com>
2022-03-13 17:37:55 -0400

    Added signup and login functionality to forms.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import LandingPage from './Components/LandingPage'
-import SignInPage from './Components/SignInPage'
-import SignUp from './Components/SignUpPage'
+import SignIn from './Components/SignIn'
+import SignUp from './Components/SignUp'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
 
 class App extends Component {
   render() {
     return (
-      <SignUp />
+      <div>
+        <Header />
+        <SignIn />
+        <Footer />
+      </div>
     )
   }
 }
--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -3,17 +3,44 @@ import Header from "./Header"
 import { Input, Button } from '@supabase/ui'
 
 class Signin extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
 
     handleOnChange = (event) => {
-        [event.target.id] = event.target.value
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+
+    submitForm = (event) => {
+        event.preventDefault()
+        fetch('http://localhost:5000/api/v1/users/login', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password
+            })
+        }).then(res => {
+            return res.json()
+        }).then(data => {
+            console.log(data)
+        })
     }
 
     render() {
         return (
             <div>
-                <Header />
                 <div class='flex items-center justify-center flex-col min-h-screen'>
-                    <form class='w-3/4 lg:w-2/3 px-8 py-10 flex items-center justify-center flex-col'>
+                    <form class='w-3/4 lg:w-2/3 px-8 py-10 flex items-center justify-center flex-col' onSubmit={this.submitForm}>
                         <div className='w-2/4 flex-col justify-items-center'>
                             <Input className='mb-6' label='User' onChange={this.handleOnChange} type='text' name='username' id='username' />
                             <Input className='mb-6' label='Password' onChange={this.handleOnChange} type='password' name='password' id='password' />
